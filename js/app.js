c6b0c739f13b0c8b136186e361a719cdc3ffde69
Nicholas <1mpuls3coding@gmail.com>
2018-07-11 22:48:14 -0700

    player can move on the board

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Place all enemy objects in an array called allEnemies
 let allEnemies = [];
 
+let lives = 3;
+
 class Enemy {
   constructor(posY, posX) {
     this.x = posX;
     this.sprite = 'images/enemy-bug.png';
     this.y = posY;
+    allEnemies.push(this);
   }
 
   render() {
@@ -25,48 +28,48 @@ Enemy.prototype.update = function(dt) {
 /* Now write your own player class
 This class requires an update(), render() and
 a handleInput() method.
-As suggested by Mathew Cranford in his article, I wrote a hero constructor
+As suggested by Mathew Cranford in his third article, I wrote a hero constructor
 class using the information provided above */
 class Hero {
   constructor() {
     this.x = 200;
     this.y = 400;
     this.sprite = 'images/char-boy.png'
-  }
+  };
 
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-  }
+  };
 
-  handleInput(key) {
-    if (key == 38) {
-      this.y += 100;
-      console.log('left');
-    } else if (key == 37) {
-      this.x -= 100;
-      console.log('up');
-    } else if (key == 39) {
-      this.x += 100;
-      console.log('right');
-    } else if (key == 40) {
-      this.y -= 100;
-      console.log('down');
+  handleInput(event) {
+    if (event == 'left' && this.x > 0) {
+      this.x -= 101;
+    } else if (event == 'up' && this.y > 0) {
+      this.y -= 83;
+    } else if (event == 'right' && this.x < 400) {
+      this.x += 101;
+    } else if (event == 'down' && this.y < 400) {
+      this.y += 83;
     }
-  }
+  };
 }
+
+// this allows the game to update the player's position
 
 Hero.prototype.update = function() {
-  
+  if (this.y < 10 ) {
+    //idfk
+  }
 }
+
 
 
 
 
 // Now instantiate your objects.
 // Place the player object in a variable called player
-const player = new Hero();
 
-// enemy objects instantiated and pushed to allEnemies array, come back and fine tune
+const player = new Hero();
 
 /* stone bug variable names are set up to have the first number referencing
 which row of stone its on, ie stoneBugOneOne is in the first row, and is the
@@ -76,15 +79,15 @@ const stoneBugOneOne = new Enemy(225, 250);
 
 const stoneBugTwoOne = new Enemy(140, 50)
 
-allEnemies.push(stoneBugOneOne);
-allEnemies.push(stoneBugTwoOne);
+const stoneBugThreeOne = new Enemy(60, -190);
+
+const stoneBugOneTwo = new Enemy(225, -100)
 
 //because there is only one grass row, its implied all grass bugs are on the
 //same row.
 
-const grassBugOne = new Enemy(300, 0);
+const grassBugOne = new Enemy(300, 25);
 
-allEnemies.push(grassBugOne);
 
 
 
@@ -97,6 +100,10 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+// collision checker
+/*if (player == enemy) {
+  game restarts
+}*/
--- js/engine.js
+++ js/engine.js
@@ -79,7 +79,7 @@ var Engine = (function(global) {
      */
     function update(dt) {
       updateEntities(dt);
-        // checkCollisions();
+      //checkCollisions();
     }
 
     /* This is called by the update function and loops through all of the
@@ -161,7 +161,6 @@ function renderEntities() {
      * those sorts of things. It's only called once by the init() method.
      */
     function reset() {
-        // noop
     }
 
     /* Go ahead and load all of the images we know we're going to need to
